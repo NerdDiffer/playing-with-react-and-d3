08cb6ee66e1a1dcdbd664f0cbdb8d5409d543150
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-10-22 18:41:49 -0700

    Setup the chart class (Scatterplot)
    Note, Scatterplot should be renamed at this point, it's supposed to
    have lines, not dots!
    
    Require the 'Lines' component. Update get*Scale methods for lines.
    Add a 'getMax' helper function.

--- unfinished/app/components/Scatterplot.jsx
+++ unfinished/app/components/Scatterplot.jsx
@@ -1,8 +1,8 @@
-var React       = require('react');
-var XYAxis      = require('./XYAxis');
+var React  = require('react');
+var XYAxis = require('./XYAxis');
+var Lines  = require('./Lines');
 
 var Scatterplot = React.createClass({
-
   render: function() {
     var props = this.props;
     var xScale = this.getXScale(props);
@@ -13,22 +13,45 @@ var Scatterplot = React.createClass({
           xScale={xScale}
           yScale={yScale}
           {...props} />
+        <Lines
+          xScale={xScale}
+          yScale={yScale}
+          {...props} />
       </svg>
     );
   },
 
   getXScale: function(props) {
-    var xMax = d3.max(props.data, function(d) { return d[0] });
-    return d3.scale.linear()
+    var xMax = props.n;
+    return d3.scale.log()
       .domain([0, xMax])
       .range([props.padding, props.width - props.padding * 2]);
   },
 
   getYScale: function(props) {
-    var yMax = d3.max(props.data, function(d) { return d[1] });
-    return d3.scale.linear()
+    var yMax = this.getMax(props.points);
+    return d3.scale.log()
       .domain([0, yMax])
       .range([props.height - props.padding, props.padding]);
+  },
+
+  // get maximum value from all arrays in the `points` object
+  getMax: function(points) {
+    var orders = Object.keys(points);
+    var n = orders.length;
+
+    if (n === 0) { return; }
+
+    var max = d3.max(points[orders[0]]);
+
+    if (n > 1) {
+      for (var i = 1; i < n; i++) {
+        var localMax = d3.max(points[orders[i]]);
+        if (localMax > max) { max = localMax; }
+      }
+    }
+
+    return max;
   }
 
 });
